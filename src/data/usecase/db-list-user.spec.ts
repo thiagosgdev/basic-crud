2ce58f564fc8a7f4cb8d0a1661ed39d1d1ad6ba4
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-16 09:13:28 -0300

    test: ensure DbListUsers returns the users on ListUsersRepository success

---
+++ src/data/usecase/db-list-user.spec.ts
@@ -0,0 +1,46 @@
+import { UserModel } from "@/domain/models/user";
+import MockDate from "mockdate";
+import { mockUsersModel } from "@/domain/tests/mock-user";
+import { ListUsersRepository } from "../protocols/list-users-repository";
+import { DbListUsers } from "./db-list-users";
+
+
+const mockListUsersRepository = (): ListUsersRepository => {
+    class ListUsersRepositoryStub implements ListUsersRepository {
+        async list(): Promise<UserModel[]> {
+            return Promise.resolve(mockUsersModel());
+        }
+    }
+    return new ListUsersRepositoryStub()
+}
+type SutTypes = {
+    sut: DbListUsers,
+    listUsersStub: ListUsersRepository
+}
+
+const makeSut = (): SutTypes => {
+    const listUsersStub = mockListUsersRepository();
+    const sut = new DbListUsers(listUsersStub);
+    return {
+        sut,
+        listUsersStub
+    }
+}
+
+describe("Db List Users", () => {
+
+    beforeAll(() => {
+        MockDate.set(new Date());
+    });
+
+    afterAll(() => {
+        MockDate.reset();
+    });
+
+    test("Should return the users on ListUsersRepository success", async () => {
+        const { sut } = makeSut();
+        const users = await sut.list();
+        expect(users.length).toBe(2);
+    });
+
+});
--- src/domain/tests/mock-user.ts
+++ src/domain/tests/mock-user.ts
@@ -19,3 +19,26 @@ export const mockUserModel = ():UserModel => ({
     created_at: new Date,
     updated_at: new Date,
 });
+
+export const mockUsersModel = ():UserModel[] => [
+    {
+        id: "any_id",
+        name: "any_name", 
+        cpf: 111111111, 
+        birthdate: new Date, 
+        cellphone: 123445678,
+        email: "any_email@mail.com",
+        created_at: new Date,
+        updated_at: new Date,
+    },
+    {
+        id: "another_id",
+        name: "another_name", 
+        cpf: 22222222, 
+        birthdate: new Date, 
+        cellphone: 987654321,
+        email: "another_email@mail.com",
+        created_at: new Date,
+        updated_at: new Date,
+    }
+];
