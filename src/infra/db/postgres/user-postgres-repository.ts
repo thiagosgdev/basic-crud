59d18a348c2e20f5a9d4e4f502f77bb04fbf7e5d
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-16 10:32:35 -0300

    feat: ensure UserPostgresRepository returns the user on find() success

--- src/infra/db/postgres/user-postgres-repository.ts
+++ src/infra/db/postgres/user-postgres-repository.ts
@@ -1,11 +1,12 @@
 import { User } from "@/domain/entities/user";
 import { UserModel } from "@/domain/models/user";
 import { AddUser, AddUserParams } from "@/domain/usecase/user/add-user";
+import { FindUser } from "@/domain/usecase/user/find-user";
 import { ListUsers } from "@/domain/usecase/user/list-users";
 import { getRepository, Repository } from "typeorm";
 
 
-export class UserPostgresRepository implements AddUser, ListUsers {
+export class UserPostgresRepository implements AddUser, ListUsers, FindUser{
     private readonly repository: Repository<User>
     constructor(){
         this.repository = getRepository(User);
@@ -26,5 +27,10 @@ export class UserPostgresRepository implements AddUser, ListUsers {
             return users;
         }
         return null;
+    }
+
+    async find(cpf: number, name?: string): Promise<UserModel> {
+        const user = await this.repository.findOne({cpf, name})
+        return user;
     }
 }
