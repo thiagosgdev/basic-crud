80fb33f5785fa669ae9f7f5ac45dac5e141db88d
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-16 14:28:25 -0300

    feat: ensure DeleteUserController calls DeleteUser with the correct values

---
+++ src/presentation/controllers/delete-user-controller.ts
@@ -0,0 +1,16 @@
+import { DeleteUser } from "@/domain/usecase/user/delete-user";
+import { Controller, HttpRequest, HttpResponse } from "../protocols";
+
+
+export class DeleteUserController implements Controller {
+    
+    constructor(
+        private readonly deleteUser: DeleteUser
+    ){}
+    
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const { cpf } = httpRequest.body;
+        await this.deleteUser.delete(cpf);
+        return null;
+    }
+}
