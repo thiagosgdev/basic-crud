8c1cbf2631b65215d8dd745e99a20ff30543cc41
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-16 12:51:45 -0300

    test: ensure UpdateUserController calls UpdateUser with the correct values

---
+++ src/presentation/controllers/update-user-controller.spec.ts
@@ -0,0 +1,64 @@
+import { UserModel } from "@/domain/models/user";
+import { mockUserModel } from "@/domain/tests/mock-user";
+import { UpdateUser, UpdateUserParams } from "@/domain/usecase/user/update-user";
+import { HttpRequest } from "../protocols";
+import { UpdateUserController } from "./update-user-controller";
+import MockDate from "mockdate";
+
+const makeFakeRequest = (): HttpRequest => ({
+    body: {
+        name: "any_name", 
+        cpf: 111111111, 
+        birthdate: new Date, 
+        cellphone: 123445678,
+        email: "another_email@mail.com",
+    }
+})
+
+const mockUpdateUser = (): UpdateUser => {
+    class UpdateUserStub implements UpdateUser {
+        update(data: UpdateUserParams): Promise<UserModel>{
+            return Promise.resolve(mockUserModel());
+        }
+    }
+    return new UpdateUserStub();
+}
+
+type SutTypes = {
+    sut: UpdateUserController,
+    updateUserStub: UpdateUser
+}
+
+const makeSut = ():SutTypes => {
+    const updateUserStub = mockUpdateUser();
+    const sut = new UpdateUserController(updateUserStub);
+    return {
+        updateUserStub,
+        sut
+    }
+}
+
+describe("Update User Controller", () => {
+
+    beforeAll(() => {
+        MockDate.set(new Date());
+    });
+
+    afterAll(() => {
+        MockDate.reset();
+    });
+
+
+    test("Should call UpdateUser with the correct values", async () => {
+        const { sut, updateUserStub } = makeSut();
+        const updateSpy = jest.spyOn(updateUserStub, "update");
+        await sut.handle(makeFakeRequest());
+        expect(updateSpy).toHaveBeenCalledWith({
+            name: "any_name", 
+            cpf: 111111111, 
+            birthdate: new Date, 
+            cellphone: 123445678,
+            email: "another_email@mail.com",
+        })
+    })
+})
