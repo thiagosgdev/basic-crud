6e1117d5b86f0bdf4d973e19020a846a2e92abe1
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-16 09:49:49 -0300

    test: ensure ListUsersController return 200 on ListUsers success

---
+++ src/presentation/controllers/list-users-controller.spec.ts
@@ -0,0 +1,36 @@
+import { UserModel } from "@/domain/models/user";
+import { mockUsersModel } from "@/domain/tests/mock-user";
+import { ListUsers } from "@/domain/usecase/user/list-users";
+import { ListUsersController } from "./list-users-controller";
+
+const mockListUsers = (): ListUsers => {
+    class ListUsersStub implements ListUsers {
+        async list(): Promise<UserModel[]> {
+            return Promise.resolve(mockUsersModel());
+        }
+    }
+    return new ListUsersStub();
+}
+
+type SutTypes = {
+    sut: ListUsersController,
+    listUsersStub: ListUsers
+}
+
+const makeSut = ():SutTypes => {
+    const listUsersStub = mockListUsers();
+    const sut = new ListUsersController(listUsersStub);
+    return {
+        listUsersStub,
+        sut,
+    }
+}
+
+describe("List Users Controller", () => {
+    test("Should return 200 on ListUsers success", async() => {
+        const { sut } = makeSut();
+        const response = await sut.handle();
+        expect(response.statusCode).toBe(200);
+    });
+
+})
