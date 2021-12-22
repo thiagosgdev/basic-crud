e1329ac500c37edec7578024f0a02a937ee1efd3
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-22 08:20:20 -0300

    feat: ensure AddUserController calls Validation with the correct values

--- src/presentation/controllers/add-user-controller.ts
+++ src/presentation/controllers/add-user-controller.ts
@@ -1,15 +1,26 @@
 import { AddUser, AddUserParams } from "@/domain/usecase/user/add-user";
 import { Controller, HttpRequest, HttpResponse } from "../protocols";
+import { Validation } from "../protocols/validation";
 
 export class AddUserController implements Controller {
 
     constructor(
-        private readonly addUser: AddUser
+        private readonly addUser: AddUser,
+        private readonly validation: Validation
     ){}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-            const  data: AddUserParams  = httpRequest.body;
+            const data: AddUserParams  = httpRequest.body;
+            const validationError = this.validation.validate(httpRequest.body);
+
+            if(validationError){
+                return {
+                    statusCode: 400,
+                    body: validationError
+                }
+            }
+            
             const user = await this.addUser.add(data);
             if(user){
                 return {
