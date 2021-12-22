cf2ed2cbbf905d2cf9d5519c7adc66f7189f0289
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-22 14:08:18 -0300

    feat: creation of email validation adapter for validator

---
+++ src/validation/validators/email-validation.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from "@/presentation/errors/invaiid-param-error";
+import { Validation } from "@/presentation/protocols/validation";
+import { EmailValidator } from "../protocols/email-validator";
+
+
+export class EmailValidation implements Validation {
+
+    constructor(
+        private readonly fieldName: string,
+        private readonly emailValidator: EmailValidator
+    ){}
+    validate(input: any): Error {
+        const isValid = this.emailValidator.isValid(input[this.fieldName]);
+        if(!isValid) {
+            return new InvalidParamError(input[this.fieldName])
+        }
+    }
+}
